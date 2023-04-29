7b8ad8a07b7a207d8756410977db25b6fed19d92
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-29 10:05:58 -0300

    resolvendo erro que estava dando de pacotes e import

--- course/src/main/java/academy/devdojo/youtube/CloudApplication.java
+++ course/src/main/java/academy/devdojo/youtube/CloudApplication.java
@@ -2,8 +2,12 @@ package academy.devdojo.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan({"com.academy.devdojo.youtube.core.model"})
+@EnableJpaRepositories({"com.academy.devdojo.youtube.core.repository"})
 public class CloudApplication {
 
 	public static void main(String[] args) {
