c3f8f49565877e6bc95d3c69c1106fd3bd3ad26f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-30 16:59:50 -0300

    criando project token

---
+++ token/src/main/java/academy/devdojo/youtube/token/TokenApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.youtube.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApplication.class, args);
+	}
+
+}
