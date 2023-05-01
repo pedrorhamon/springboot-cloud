33c76f4e3ddea7726a9d87bc4d4d50db41f68133
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-30 22:51:56 -0300

    estou tendo problema de port, ja troquei mas o erro ainda persiste

--- course/src/main/java/academy/devdojo/youtube/CloudApplication.java
+++ course/src/main/java/academy/devdojo/youtube/CloudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +13,7 @@ import academy.devdojo.youtube.core.property.JwtConfiguration;
 @SpringBootApplication
 @EntityScan({"academy.devdojo.youtube.core.model"})
 @EnableJpaRepositories({"academy.devdojo.youtube.core.repository"})
+@EnableEurekaClient
 @EnableConfigurationProperties(value = JwtConfiguration.class)
 @ComponentScan("academy.devdojo.youtube")
 public class CloudApplication {
