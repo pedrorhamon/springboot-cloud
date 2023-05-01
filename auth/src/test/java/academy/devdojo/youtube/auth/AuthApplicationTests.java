2a535d17d203c27b69c908699feb001a270c8757
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-30 23:52:45 -0300

    ainda estou tendo problema de version

--- auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
+++ auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
@@ -1,16 +1,13 @@
 package academy.devdojo.youtube.auth;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author pedroRhamon
  *
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthApplicationTests {
 	
